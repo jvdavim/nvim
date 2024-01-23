f5d3e093b8303cf3087eff5338085ebea1b4c79c
Davim, Joao <joao.davim@br.experian.com>
2024-01-23 09:05:48 -0300

    load dapui eager

---
+++ after/plugin/dapui.lua
@@ -0,0 +1,15 @@
+local dap = require("dap")
+local dapui = require("dapui")
+dapui.setup()
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
+vim.keymap.set("n", "<leader>b", "<cmd> DapToggleBreakpoint <CR>")
+vim.keymap.set("n", "<leader>db", "<cmd> DapContinue <CR>")
+require("dap.ext.vscode").load_launchjs()
--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -1,21 +1,4 @@
 return {
     "rcarriga/nvim-dap-ui",
     dependencies = "mfussenegger/nvim-dap",
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
-        dapui.setup()
-        dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close()
-        end
-        vim.keymap.set("n", "<leader>b", "<cmd> DapToggleBreakpoint <CR>")
-        vim.keymap.set("n", "<leader>db", "<cmd> DapContinue <CR>")
-        require("dap.ext.vscode").load_launchjs()
-    end,
 }
