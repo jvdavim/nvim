f99ce575d47f805f53ca4612eb087a37cd0e66fe
jvdavim <jvdavim@gmail.com>
2024-05-11 11:34:34 -0300

    set larger width harpoon

--- lua/jvdavim/plugins/harpoon.lua
+++ lua/jvdavim/plugins/harpoon.lua
@@ -1,10 +1,15 @@
 return {
-	"ThePrimeagen/harpoon",
-	config = function()
-		local mark = require("harpoon.mark")
-		local ui = require("harpoon.ui")
+    "ThePrimeagen/harpoon",
+    config = function()
+        require("harpoon").setup({
+            menu = {
+                width = vim.api.nvim_win_get_width(0) - 100,
+            }
+        })
+        local mark = require("harpoon.mark")
+        local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-	end,
+        vim.keymap.set("n", "<leader>a", mark.add_file)
+        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+    end,
 }
