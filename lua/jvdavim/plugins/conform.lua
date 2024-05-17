3836f69d2686a378b61780be4226d60223a1498a
Davim, Joao <joao.davim@br.experian.com>
2024-05-17 16:35:36 -0300

    show hidden files in telescope

--- lua/jvdavim/plugins/conform.lua
+++ lua/jvdavim/plugins/conform.lua
@@ -1,53 +1,53 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>fw",
-			function()
-				require("conform").format({ lsp_fallback = true, async = true })
-			end,
-			mode = "",
-			desc = "Format file or range (in visual mode)",
-		},
-	},
-	opts = {
-		formatters_by_ft = {
-			javascript = { { "eslint_d", "prettierd", "prettier" } },
-			typescript = { { "eslint_d", "prettierd", "prettier" } },
-			javascriptreact = { { "prettierd", "prettier" } },
-			typescriptreact = { { "prettierd", "prettier" } },
-			svelte = { { "prettierd", "prettier" } },
-			css = { { "prettierd", "prettier" } },
-			html = { { "prettierd", "prettier" } },
-			json = { { "jq", "prettierd", "prettier" } },
-			yaml = { { "prettierd", "prettier" } },
-			markdown = { { "prettierd", "prettier" } },
-			graphql = { { "prettierd", "prettier" } },
-			csharp = { { "dotnet-csharpier" } },
-			lua = { "stylua" },
-			python = function(bufnr)
-				if require("conform").get_formatter_info("ruff_format", bufnr).available then
-					return { "isort", "ruff_format" }
-				else
-					return { "isort", "black" }
-				end
-			end,
-			go = { "gofmt", "gofumpt" },
-			bash = { "shfmt" },
-			sh = { "shfmt" },
-		},
-		format_on_save = false,
-	},
-	config = function()
-		require("conform.formatters.eslint_d").cwd = require("conform.util").root_file({
-			".eslint.js",
-			".eslint.cjs",
-			".eslint.yaml",
-			".eslint.yml",
-			".eslint.json",
-		})
-		require("conform.formatters.eslint_d").require_cwd = true
-	end,
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>fw",
+            function()
+                require("conform").format({ lsp_fallback = true, async = true })
+            end,
+            mode = "",
+            desc = "Format file or range (in visual mode)",
+        },
+    },
+    opts = {
+        formatters_by_ft = {
+            javascript = { "eslint_d", { "prettierd", "prettier" } },
+            typescript = { "eslint_d", { "prettierd", "prettier" } },
+            javascriptreact = { { "prettierd", "prettier" } },
+            typescriptreact = { { "prettierd", "prettier" } },
+            svelte = { { "prettierd", "prettier" } },
+            css = { { "prettierd", "prettier" } },
+            html = { { "prettierd", "prettier" } },
+            json = { { "jq", "prettierd", "prettier" } },
+            yaml = { { "prettierd", "prettier" } },
+            markdown = { { "prettierd", "prettier" } },
+            graphql = { { "prettierd", "prettier" } },
+            csharp = { { "dotnet-csharpier" } },
+            lua = { "stylua" },
+            python = function(bufnr)
+                if require("conform").get_formatter_info("ruff_format", bufnr).available then
+                    return { "isort", "ruff_format" }
+                else
+                    return { "isort", "black" }
+                end
+            end,
+            go = { { "gofmt", "gofumpt" } },
+            bash = { "shfmt" },
+            sh = { "shfmt" },
+        },
+        format_on_save = false,
+    },
+    config = function()
+        require("conform.formatters.eslint_d").cwd = require("conform.util").root_file({
+            ".eslint.js",
+            ".eslint.cjs",
+            ".eslint.yaml",
+            ".eslint.yml",
+            ".eslint.json",
+        })
+        require("conform.formatters.eslint_d").require_cwd = true
+    end,
 }
