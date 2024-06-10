8056ddd0ab5809518125f030bb1c5838df7130f9
Davim, Joao <joao.davim@br.experian.com>
2024-06-10 17:28:34 -0300

    setup dapui layout

--- lua/jvdavim/plugins/dap-ui.lua
+++ lua/jvdavim/plugins/dap-ui.lua
@@ -4,7 +4,46 @@ return {
     config = function()
         local dap = require("dap")
         local dapui = require("dapui")
-        dapui.setup()
+        dapui.setup({
+            layouts = {
+                {
+                    elements = {
+                        {
+                            id = "scopes",
+                            size = 0.25
+                        },
+                        {
+                            id = "breakpoints",
+                            size = 0.25
+                        },
+                        {
+                            id = "stacks",
+                            size = 0.25
+                        },
+                        {
+                            id = "watches",
+                            size = 0.25
+                        }
+                    },
+                    position = "left",
+                    size = 40
+                },
+                {
+                    elements = {
+                        {
+                            id = "repl",
+                            size = 1
+                        },
+                        -- {
+                        --     id = "console",
+                        --     size = 0.1
+                        -- },
+                    },
+                    position = "bottom",
+                    size = 10
+                },
+            },
+        })
         dap.listeners.after.event_initialized["dapui_config"] = function()
             dapui.open()
         end
