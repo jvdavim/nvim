289a693390eb64ff11bbfebcfbdd6b96c257e5be
Davim, Joao <joao.davim@br.experian.com>
2024-07-10 14:49:32 -0300

    add full path to lualine

--- lua/jvdavim/plugins/lualine.lua
+++ lua/jvdavim/plugins/lualine.lua
@@ -5,6 +5,16 @@ return {
         require("lualine").setup({
             options = {
                 theme = "horizon"
+            },
+            sections = {
+                lualine_c = { {
+                    'filename',
+                    path = 2,
+                    fmt = function(path)
+                        return table.concat({ vim.fs.dirname(path),
+                            vim.fs.basename(path) }, package.config:sub(1, 1))
+                    end
+                } },
             }
         })
     end
