0eb4e6acea08e781322b42ce9e598129ebed2f75
Davim, Joao <joao.davim@br.experian.com>
2024-09-05 14:07:36 -0300

    add virt columns ui

--- lua/jvdavim/options.lua
+++ lua/jvdavim/options.lua
@@ -20,4 +20,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "+1,80,120"
+-- vim.opt.colorcolumn = "+1,80,120"
+
+vim.opt.list = true
+vim.opt.lcs = "tab:»·,trail:·,space:·"
