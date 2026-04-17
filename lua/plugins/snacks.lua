3e80d5420e997b255f86422f0b1f48afe3222737
Davim, Joao <joao.davim@experian.com>
2026-04-17 12:09:55 -0300

    fix snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,8 @@ local spec = { "folke/snacks.nvim" }
 vim.pack.add({ spec[1] })
 
 pcall(function()
-    require("snacks").setup()
+    require("snacks").setup({
+        input = {},
+        picker = {},
+    })
 end)
